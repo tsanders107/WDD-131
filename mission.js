cb9f8a3b64685e7bfd61c0fb177016c7728a4c0b
tsanders107 <145694545+tsanders107@users.noreply.github.com>
2026-01-17 16:42:31 -0700

    Add files via upload

---
+++ mission.js
@@ -0,0 +1,17 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+}           
+
+
+                    
